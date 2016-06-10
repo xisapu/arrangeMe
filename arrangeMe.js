bfb23a874f6c68ff89805bd7a7a150758d8fabe7
xisapu <xisapu@mswork.ru>
2016-06-10 09:48:11 +0300

    added basic gameboard

--- arrangeMe.js
+++ arrangeMe.js
@@ -1,3 +1,14 @@
-function onArrangeMeload() {
-	document.getElementById("g").textContent="secret"
+function buildGameBoard(number_of_blocks_in_side) {
+	var table = document.getElementById("gameBoard");
+	var counter = number_of_blocks_in_side * number_of_blocks_in_side;
+	for (var i = 0; i < number_of_blocks_in_side; i++) {
+		var row = table.insertRow(0);
+		for (var j = 0; j < number_of_blocks_in_side; j++) {
+			var cell = row.insertCell(0);
+			cell.className="game_block"
+			cell.textContent = counter;
+			counter--;
+		}
+	}
+	table.rows[number_of_blocks_in_side - 1].cells[number_of_blocks_in_side - 1].textContent = "";
 }
