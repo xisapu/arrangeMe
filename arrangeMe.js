d7b0956e26470d11cc47bd22092bf80cbd799b91
xisapu <xisapu@mswork.ru>
2016-06-13 17:51:00 +0300

    added another input way

--- arrangeMe.js
+++ arrangeMe.js
@@ -4,7 +4,8 @@ var image_src = "http://fc00.deviantart.net/fs70/f/2013/359/b/7/anime_quote__138
 
 function setup(number_of_blocks_in_side) {
 	buildGameBoardWithImage(number_of_blocks_in_side);
-	scrumbleBoard();
+	//buildGameBoardWithNumbers(number_of_blocks_in_side);
+	//scrumbleBoard();
 }
 
 
@@ -27,32 +28,41 @@ function buildGameBoardWithNumbers(number_of_blocks_in_side) {
 
 
 function buildGameBoardWithImage(number_of_blocks_in_side) {
-	var image_side_size = 500;
-	var block_side_size = image_side_size / number_of_blocks_in_side;
+	var image = document.getElementById("hiddenImage");
+	var image_X_width = image.width;
+	var image_Y_height = image.height;
+	var block_X_size = image_X_width / number_of_blocks_in_side;
+	var block_Y_size = image_Y_height / number_of_blocks_in_side;
 	var FromX = 0;
 	var FromY = 0;
-	var ToX = block_side_size;
-	var ToY = block_side_size;
+	var ToX = block_X_size;
+	var ToY = block_Y_size;
 	var table = document.getElementById("gameBoard");
 	var counter = number_of_blocks_in_side * number_of_blocks_in_side;
+	table.style.width = image.width + "px";
+	table.style.height = image.height + "px";
 	for (var i = 0; i < number_of_blocks_in_side; i++) {
 		var row = table.insertRow(0);
 		var FromX = 0;
-		var ToX = block_side_size;
+		var ToX = block_X_size;
 		for (var j = 0; j < number_of_blocks_in_side; j++) {
 			var cell = row.insertCell(0);
 			cell.className = "game_block";
 			cell.id = counter;
-			cell.textContent = "<img id=i'" + counter + "' class='imageToCrop' src='" + image_src + "'";
-			var cellImage = document.getElementById("i" + counter);
-			cellImage.style.clip = "rect('" + FromY + "px', " + ToX + "px, " + ToY + "px, " + FromX + "px)";
+			cell.appendChild(document.createElement("img"))
+			var cellImage = cell.getElementsByTagName("img")[0];
+			cellImage.id = "i" + counter;
+			cellImage.className = "cropedImage";
+			cellImage.src = image_src;
+			var rect_string = "rect(" + FromY + "px, " + ToX + "px, " + ToY + "px, " + FromX + "px)";
+			cellImage.style.clip = "rect(" + FromY + "px, " + ToX + "px, " + ToY + "px, " + FromX + "px)";
 			cell.onclick = function() {moveBlock(this)};
-			FromX += block_side_size;
-			ToX += block_side_size;
+			FromX += block_X_size;
+			ToX += block_X_size;
 			counter--;
 		}
-		FromY += block_side_size;
-		ToY += block_side_size;
+		FromY += block_Y_size;
+		ToY += block_Y_size;
 	}
 	table.rows[number_of_blocks_in_side - 1].cells[number_of_blocks_in_side - 1].textContent = "";
 }
@@ -60,7 +70,7 @@ function buildGameBoardWithImage(number_of_blocks_in_side) {
 
 function checkIfBlockEmpty(id) {
 	var block = document.getElementById(id);
-	if (block.textContent == "") {
+	if ((block.textContent == "") && (block.children == [])) {
 		return true;
 	}
 	return false;
@@ -104,15 +114,21 @@ function getEmptyCoupledBlocksId(id) {
 }
 
 
+function switchBlocksContent(block1, block2) {
+	var temp_text_content = block1.textContent;
+	block1.textContent = block2.textContent;
+	block2.textContent = temp_text_content;
+}
+
+
 function moveBlock(cellObject) {
 	var empty_coupled_block_id = getEmptyCoupledBlocksId(cellObject.id);
 	if (empty_coupled_block_id) {
 		var illegalMove = document.getElementById("illegalMove");
 		illegalMove.style.display = "none";
-		actions_history.push(empty_coupled_block_id)
+		actions_history.push(empty_coupled_block_id);
 		var empty_coupled_block = document.getElementById(empty_coupled_block_id);
-		empty_coupled_block.textContent = cellObject.textContent;
-		cellObject.textContent = "";
+		switchBlocksContent(cellObject, empty_coupled_block)
 		var winMessage = document.getElementById("winMessage");
 		var is_board_arranged = checkIfBoardIsArranged();
 		if (is_board_arranged) {
@@ -162,7 +178,6 @@ function scrumbleBoard() {
 	for (var i = 0; i < number_of_moves; i++) {
 		var random_side = Math.floor((Math.random() * 4) + 1);
 		var sides_dictionary = createsSidesDictionary();
-		//switch with upper block
 		if (sides_dictionary[random_side] === "upper_block") {
 			if (empty_cell_id > row_length) {
 				empty_cell_id = empty_cell_id - row_length;
@@ -170,7 +185,6 @@ function scrumbleBoard() {
 				moveBlock(upper_block);
 			}
 		}
-		//switch with bottom block
 		if (sides_dictionary[random_side] === "bottom_block") {
 			if (empty_cell_id <= number_of_cells - row_length) {
 				empty_cell_id = empty_cell_id + row_length;
@@ -178,7 +192,6 @@ function scrumbleBoard() {
 				moveBlock(bottom_block);
 			}
 		}
-		//switch with left block
 		if (sides_dictionary[random_side] === "left_block") {
 			if (empty_cell_id % row_length !== 1) {
 				empty_cell_id = empty_cell_id - 1
@@ -186,7 +199,6 @@ function scrumbleBoard() {
 				moveBlock(left_block);
 			}
 		}
-		//switch with right block
 		if (sides_dictionary[random_side] === "right_block") {
 			if (empty_cell_id % row_length !== 0) {
 				empty_cell_id = empty_cell_id + 1;
@@ -204,5 +216,74 @@ function undoAction() {
 		var block = document.getElementById(actions_history.pop());
 		moveBlock(block);
 		actions_history.pop()
+	}
+}
+
+
+function createsKeysDictionary() {
+	var keys_dictionary = {};
+	keys_dictionary['ArrowDown'] = "switch upper block";
+	keys_dictionary['s'] = "switch upper block";
+	keys_dictionary['S'] = "switch upper block";
+	keys_dictionary['ArrowUp'] = "switch bottom block";
+	keys_dictionary['w'] = "switch bottom block";
+	keys_dictionary['W'] = "switch bottom block";
+	keys_dictionary['ArrowRight'] = "switch left block";
+	keys_dictionary['d'] = "switch left block";
+	keys_dictionary['D'] = "switch left block";
+	keys_dictionary['ArrowLeft'] = "switch right block";
+	keys_dictionary['a'] = "switch right block";
+	keys_dictionary['A'] = "switch right block";
+	return keys_dictionary;
+}
+
+
+function findEmptyBlockId() {
+	var table = document.getElementById("gameBoard");
+	var row_length = table.rows.length;
+	var number_of_cells = row_length * row_length;
+	for (var i = 1; i <= number_of_cells; i++) {
+		var block = document.getElementById(i);
+		if (block.textContent === "") {
+			return i;
+		}
+	}
+}
+
+
+function keyPressManager(event) {
+	var key = event.key;
+	var keys_dictionary = createsKeysDictionary();
+	var empty_block_id = findEmptyBlockId();
+	var table = document.getElementById("gameBoard");
+	var row_length = table.rows.length;
+	var number_of_cells = row_length * row_length;
+	if (keys_dictionary[key] === "switch upper block") {
+		if (empty_block_id > row_length) {
+			empty_block_id = empty_block_id - row_length;
+			upper_block = document.getElementById(empty_block_id);
+			moveBlock(upper_block);
+		}
+	}
+	if (keys_dictionary[key] === "switch bottom block") {
+		if (empty_block_id <= number_of_cells - row_length) {
+			empty_block_id = empty_block_id + row_length;
+			bottom_block = document.getElementById(empty_block_id);
+			moveBlock(bottom_block);
+		}
+	}
+	if (keys_dictionary[key] === "switch left block") {
+		if (empty_block_id % row_length !== 1) {
+			empty_block_id = empty_block_id - 1
+			left_block = document.getElementById(empty_block_id);
+			moveBlock(left_block);
+		}
+	}
+	if (keys_dictionary[key] === "switch right block") {
+		if (empty_block_id % row_length !== 0) {
+			empty_block_id = empty_block_id + 1;
+			right_block = document.getElementById(empty_block_id);
+			moveBlock(right_block);
+		}
 	}
 }
